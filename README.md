fd3e79e883b7dce16f78a88ccaea44ed0a3a52de
ColinRhys <31706927+ColinRhys@users.noreply.github.com>
2024-04-16 10:53:13 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Example project using rust Kafka project using the rdkafka crate and tokio
+
+To test run "docker-compose up" and then "cargo run" to see test output
+
+"Message Sent (0, 2)
+Message Consumed : Hello World, I am testing"
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use producer::produce;
+
+mod producer;
+mod consumer;
+
+#[tokio::main]
+async fn main() {
+    //Kafka - Message Queue
+    let producer = producer::create();
+    produce(producer, String::from("Hello World, I am testing")).await;
+
+    consumer::start().await;
+}
